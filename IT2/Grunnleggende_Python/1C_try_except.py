ffa10033d2a8fb561fe0939cb376d22c89e9cd7b
uwohlen <112326477+uwohlen@users.noreply.github.com>
2026-09-03 09:42:38 +0200

    IT2 try except updated

--- IT2/Grunnleggende_Python/1C_try_except.py
+++ IT2/Grunnleggende_Python/1C_try_except.py
@@ -1,14 +1,12 @@
 svar = input("skriv noe: ")
 
 try:
-  print("det var et tall")
   tall = float(svar)
 except:
   print("det var ikke et tall")
 else:
-  pass
+  print("det var et tall")
 finally:
-  pass
+  print("testen er ferdig")
 
-print("testen er ferdig")
 print("dette er ikke en del av try-except")
